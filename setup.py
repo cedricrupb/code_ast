9e34eea3acccf4a6c497c46398de29a00445c272
Cedric Richter <cedricr@mail.upb.de>
2022-05-09 14:49:53 +0200

    Added necessary files to install code_ast

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+  name = 'code_ast',
+  packages = ['code_ast'], 
+  version = '0.1.0', 
+  license='MIT',     
+  description = 'Fast structural analysis of any programming language in Python',
+  long_description = long_description,
+  long_description_content_type="text/markdown",
+  author = 'Cedric Richter',                   
+  author_email = 'cedricr.upb@gmail.com',    
+  url = 'https://github.com/cedricrupb/code_ast',  
+  download_url = '', 
+  keywords = ['code', 'ast', 'syntax', 'program', 'language processing'], 
+  install_requires=[          
+          'tree_sitter',
+          'GitPython',
+          'requests'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',    
+    'Intended Audience :: Developers',  
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3', 
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
